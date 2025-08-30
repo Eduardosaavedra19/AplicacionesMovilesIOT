22d29fa095d3d05a47f5573088d01d1da0ca16de
Eduardosaavedra19 <eduardo.saavedra19@inacapmail.cl>
2025-08-29 20:01:17 -0400

    primera_prueba

--- bin/proyecto_uno.dart
+++ bin/proyecto_uno.dart
@@ -1,5 +1,15 @@
-import 'package:proyecto_uno/proyecto_uno.dart' as proyecto_uno;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${proyecto_uno.calculate()}!');
+  stdout.write("Indicame tu nombre: ");
+  var nombre =stdin.readLineSync();
+  stdout.write("Indicame tu edad: ");
+  var edad =stdin.readLineSync();
+  int edadInt= int.parse(edad.toString());
+  stdout.write("Indicame tu Peso: ");
+  String? peso =stdin.readLineSync();
+  double pesoNum= double.parse(peso.toString());
+  //bool activo = true;
+
+  print("El nombre del usuario es $nombre, su edad es $edadInt, su peso es $pesoNum");
 }
